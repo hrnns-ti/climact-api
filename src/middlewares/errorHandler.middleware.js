89a5aab3dc9d56ee78d517086251e3d712b4699e
Haerunnas <nassjourney@gmail.com>
2025-10-12 21:20:56 +0700

    Remove all files except README.md for new backend push

--- src/middlewares/errorHandler.middleware.js
+++
@@ -1,7 +0,0 @@
-export function errorHandler(err, req, res, next) {
-    console.error(err); // log error ke console/server log
-    res.status(err.status || 500).json({
-        success: false,
-        error: err.message || 'Internal Server Error'
-    });
-}
